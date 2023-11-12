f97b347306dee1be9608b3889f8ccb4b4a831440
Paweł <pawellinek3d@gmail.com>
2023-11-12 19:37:47 +0100

    implemented String method for gentle writing

--- ast/ast.go
+++ ast/ast.go
@@ -1,21 +1,29 @@
 package ast
 
-import "interpreter/token"
+import (
+	"bytes"
+	"interpreter/token"
+)
 
+// Node represents a generic node in the abstract syntax tree (AST).
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
+// Statement represents an AST node that is a statement.
 type Statement interface {
 	Node
 	StatementNode()
 }
 
+// Expression represents an AST node that is an expression.
 type Expression interface {
 	Node
 	expressionNode()
 }
 
+// Program represents the root of the AST, containing a list of statements.
 type Program struct {
 	Statements []Statement
 }
@@ -28,42 +36,93 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// LetStatement represents a variable assignment statement.
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
 	Value Expression
 }
 
-func (ls *LetStatement) StatementNode() {
-
-}
+func (ls *LetStatement) StatementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// Identifier represents an identifier expression.
 type Identifier struct {
-	Token token.Token // IDENT
+	Token token.Token
 	Value string
 }
 
-func (i *Identifier) expressionNode() {
-
-}
+func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Token.Literal
+}
+
+// ReturnStatement represents a return statement.
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) StatementNode() {
+func (rs *ReturnStatement) StatementNode() {}
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
 }
 
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+// ExpressionStatement represents a statement consisting of a single expression.
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) StatementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
 }
