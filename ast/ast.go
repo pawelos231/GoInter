02cd84e9ec27b0c46fcd192be1634ea9216b207c
Paweł <pawellinek3d@gmail.com>
2024-01-11 15:22:49 +0100

    function literal declaration in ast

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"interpreter/token"
+	"strings"
 )
 
 // Node represents a generic node in the abstract syntax tree (AST).
@@ -75,7 +76,7 @@ func (ls *LetStatement) String() string {
 	return out.String()
 }
 
-// Identifier represents an identifier expression.
+// Identifier represents an identifier expression, like age in: let age = 5.
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -262,5 +263,36 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {
+
+}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ","))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
 	return out.String()
 }
